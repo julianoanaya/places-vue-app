5b36fc177ca2644e1ec1f6bb873458522ad9c042
Julian Anaya <juliananaya531@gmail.com>
2022-06-12 08:38:43 -0700

    added create and show action cant vue since rack-cors is blocking

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,6 +7,8 @@ export default {
       message: "Welcome to Vue.js!",
       places: [],
       newPlacesParams: {},
+      editPlaceParams: {},
+      currentPlace: {},
     };
   },
   created: function () {
@@ -19,6 +21,25 @@ export default {
         this.places = response.data;
       });
     },
+    createPlace: function () {
+      console.log("Creating place...");
+
+      var params = this.newPlaceParams;
+
+      axios
+        .post("http://localhost:3000/places.json", params)
+        .then((response) => {
+          console.log("Created Place", response.data);
+          this.place.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log("info is shown", place);
+      this.currentPlace = place;
+      this.editPlaceParams = place;
+      document.querySelector("#place-details").showModal();
+    },
   },
 };
 </script>
@@ -26,10 +47,27 @@ export default {
 <template>
   <div class="home">
     <h1>All places</h1>
+    <h2>New Place</h2>
+    <div>
+      Name:
+      <input type="text" v-model="newPlacesParams.name" />
+    </div>
+    <div>
+      Price:
+      <input type="text" v-model="newPlacesParams.address" />
+    </div>
+    <button v-on:click="createProduct()">Create Product</button>
     <div v-for="place in places" v-bind:key="place.id">
       <h2>{{ place.name }}</h2>
       <h2>{{ place.address }}</h2>
     </div>
+    <dialog id="place-details">
+      <form method="dialog">
+        <h2>Place info</h2>
+        <p>Name: {{ currentPlace.name }}</p>
+        <p>Address: {{ currentPlace.address }}</p>
+      </form>
+    </dialog>
   </div>
 </template>
 
