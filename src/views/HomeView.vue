d91c4871ee2b6537fb4250bb5b12f783259144a1
Julian Anaya <juliananaya531@gmail.com>
2022-06-12 08:18:58 -0700

    added index action got stcuk since page would not load index action due to rack-cors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,36 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
-</template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
 
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Welcome to Vue.js!",
+      places: [],
+      newPlacesParams: {},
+    };
+  },
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("/places").then((response) => {
+        console.log("places index", response);
+        this.places = response.data;
+      });
+    },
   },
 };
 </script>
+
+<template>
+  <div class="home">
+    <h1>All places</h1>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2>{{ place.name }}</h2>
+      <h2>{{ place.address }}</h2>
+    </div>
+  </div>
+</template>
+
+<style></style>
